024dfd875fff63588cba82baa24e68b64fe6fdc3
Kevin Colten <kevincolten@gmail.com>
2016-10-16 21:51:03 -0500

    config taking environment variables

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('PSM_DB_HOST', getenv('PSM_DB_HOST'));
+define('PSM_DB_NAME', getenv('PSM_DB_NAME'));
+define('PSM_DB_USER', getenv('PSM_DB_USER'));
+define('PSM_DB_PASS', getenv('PSM_DB_PASS'));
+define('PSM_DB_PREFIX', 'psm_');
